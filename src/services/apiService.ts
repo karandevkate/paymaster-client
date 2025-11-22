ce7162a9e7d38ad9c821822869344bef8e2a86c4
mrkarandevkate <karandevkate225@gmail.com>
2025-11-22 20:20:52 +0530

    Setup PayMaster frontend project and prepare for deployment

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { UUID } from 'crypto';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080/api',
+    baseURL: process.env.REACT_APP_API_BASE_URL,
     headers: {
         'Content-Type': 'application/json',
     },
